74f69eeba0e0ab78ed4f4b775a0b78e52c2767d5
marcus <mmxo5813@gmail.com>
2023-05-25 01:15:45 +1000

    api: submit form including image

--- deepracer-fe/src/components/ScoreEntry.jsx
+++ deepracer-fe/src/components/ScoreEntry.jsx
@@ -1,4 +1,4 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import { useDispatch } from "react-redux";
 import { addScore } from "../redux/scoreSlice";
 
@@ -6,10 +6,23 @@ import { useSelector } from "react-redux";
 
 import "../styles/scoreentry.scss";
 
-
 function ScoreEntry() {
   const nameRef = useRef();
   const timeRef = useRef();
+
+  const [evalCSV, setEvalCSV] = useState();
+  const [trainingCSV, setTrainingCSV] = useState();
+  const [evalLog, setEvalLog] = useState();
+
+  const handleEvalCSVChange = (e) => {
+    setEvalCSV(e.target.files[0]);
+  }
+  const handleTrainingCSVChange = (e) => {
+    setTrainingCSV(e.target.files[0]);
+  }
+  const handleEvalLogChange = (e) => {
+    setEvalLog(e.target.files[0]);
+  }
 
   const dispatch = useDispatch();
   const handleSubmit = (e) => {
@@ -17,13 +30,31 @@ function ScoreEntry() {
     dispatch(
       addScore({ name: nameRef.current.value, time: timeRef.current.value })
     );
+    
+    const data = new FormData();
+    // if (nameRef.current.value) 
+    data.append("modelname", nameRef.current.value);
+    // if (evalCSV)
+    data.append("evalcsv", evalCSV);
+    // if (trainingCSV)
+    data.append("trainingcsv", trainingCSV);
+
+    data.append("evallog", evalLog);
+    
+    fetch("http://localhost:4000/api/csvtest", {
+      method: "POST",
+      body: data,
+    })
+    .then((res) => res.json())
+      .then((data) => console.log(data))
+      .catch((err) => console.error(err));
+    
     nameRef.current.value = "";
     timeRef.current.value = "";
   };
 
   const allScores = useSelector((state) => state.score.scores);
   console.log({allScores});
-
 
   return (
     <>
@@ -33,16 +64,16 @@ function ScoreEntry() {
           <input placeholder="Model Name" ref={nameRef} />
           <input placeholder="Time in seconds" ref={timeRef} />
           <div className="score-form-input">
-            <label for="">Evaluation CSV</label>
-            <input type="file" onChange={() => console.log("afsas")} />
+            <label>Evaluation CSV</label>
+            <input type="file" onChange={handleEvalCSVChange} />
           </div>
           <div className="score-form-input">
-            <label for="">Training CSV</label>
-            <input type="file" onChange={() => console.log("afsas")} />
+            <label>Training CSV</label>
+            <input type="file" onChange={handleTrainingCSVChange} />
           </div>
           <div className="score-form-input">
-            <label for="">Evaluation Logs</label>
-            <input type="file" onChange={() => console.log("afsas")} />
+            <label>Evaluation Log</label>
+            <input type="file" onChange={handleEvalLogChange} />
           </div>
           <button type="submit">Submit</button>
         </form>
